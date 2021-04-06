4fea7b6bc2b7f7a2e8b71f5c3cf6b81c264f719f
lo0rd <lo0rd1@bk.ru>
2021-04-06 20:31:28 +0300

    initial commit

---
+++ Arrays.js
@@ -0,0 +1,54 @@
+"use strict"
+let arr_1 = [1, 3, 4];
+let arr_2 = arr_1;
+
+arr_2.push(78);
+
+
+// плохо, если с массивом работать как с обычным объектом
+
+for ( let i = 0; i < 10e1; i++) {
+    arr_1[i] = +(Math.random() * 10).toFixed();
+}
+
+// for(let item of arr_2) {
+//     console.log( item );
+// }
+// console.log('=====================================================');
+// for (const key in arr_2) {
+//     console.log( arr_2[key] );
+// }
+
+// let obj = {
+//     first : 12,
+//     second : 90,
+// }
+
+// for (let index in arr_2) {
+//     //console.log(index);
+// }
+
+// let arr = new Array(3); // создается пустой массив длины 3
+
+// let styles = ["Джаз", "Блюз", 1, 8];
+// styles.push("Рок-н-ролл");
+// console.log(styles);
+// styles[Math.floor(styles.length / 2)] = "Классика";
+// console.log(styles);
+// console.log(styles.shift());
+// styles.unshift("Рэп", "Регги")
+// console.log(styles);
+
+function sumInput(){
+    let inputValue;
+
+    do{
+        inputValue = prompt("Ввелите число", 0);
+        alert(inputValue);
+    }while(inputValue != null)
+}
+
+
+
+
+
